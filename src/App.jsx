603e6538ef9d593fc8ce8dc39d1b00802521320a
hyejin <po852alth@naver.com>
2024-06-13 10:36:04 +0900

    add: queryClient 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Router from "./shared/Router";
 import GlobalStyles from "./shared/GlobalStyles";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <Router />
-      <ToastContainer autoClose={700} position="top-left" />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <Router />
+        <ToastContainer autoClose={700} position="top-left" />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+
+const StForm = styled.form`
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+
+  input {
+    width: 300px;
+    height: 40px;
+  }
+
+  button {
+    width: 300px;
+    height: 40px;
+  }
+`;
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <>
+      <StForm>
+        <p>프로필 수정</p>
+        <input type="text" />
+        <p>아바타 이미지</p>
+        <input type="file" accept="image/*" />
+        <button>프로필 업데이트</button>
+      </StForm>
+    </>
+  );
 }
 
 export default Profile;
